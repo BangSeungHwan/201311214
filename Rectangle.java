2fbce8640a78c285f939ac5aebbb3dbe95d1aecd
201311214 방승환 <goodjob930@naver.com>
2016-10-03 14:46:24 +0900

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,32 @@
+class Rectangle {
+  private int width;
+  private int height;
+  public Rectangle(int w, int h) {
+    this.width = w;
+    this.height = h;
+  }
+  public int getWidth() {
+    return width;
+  }
+  public int getHeight() {
+    return height;
+  }
+  public void setHeight(int h) {
+    height = h;
+  }
+  public void setWidth(int newWidth) {
+    width = newWidth;
+  }
+  public void setSize(int w, int h) {
+    setWidth(w);
+    setHeight(h);
+  }
+  public int calcArea() {
+    return width*height;
+  }
+}
+
+Rectangle rectangle = new Rectangle(3,6);
+System.out.println("면적 : " + rectangle.calcArea());
+rectangle.setSize(5,8);
+System.out.println("면적 : " + rectangle.calcArea());
